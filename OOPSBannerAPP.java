46b4eae122320738fa2015dbf7ebf57abf3431e9
sriramsaimedapati <sriramsaimedapati@gmail.com>
2026-02-17 20:59:05 +0530

    UC2: Implemented ASCII OOPS banner using manual prints

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,15 +1,14 @@
-/**
- * OOPSBannerApp.java
- * 
- * This program prints the literal text "OOPS" to the console.
- * 
- * @author Sriram
- * @version 1.0
- */
-public class OOPSBannerAPP{
-    
+public class OOPSBannerAPP {
+
     public static void main(String[] args) {
-        // Print the literal "OOPS"
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *****   *****");
+        System.out.println("*     * *     * *         *  ");
+        System.out.println("*     * *     * *         *  ");
+        System.out.println(" *****   *****   *         *****");
+
     }
 }
