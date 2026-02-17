ca77a6704fd1c7e6955bdab0eecc2d0d9994f20e
sriramsaimedapati <sriramsaimedapati@gmail.com>
2026-02-17 15:43:57 +0530

    UC1: Print OOPS

---
+++ OOPSBannerAPP.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp.java
+ * 
+ * This program prints the literal text "OOPS" to the console.
+ * 
+ * @author Sriram
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    
+    public static void main(String[] args) {
+        // Print the literal "OOPS"
+        System.out.println("OOPS");
+    }
+}
