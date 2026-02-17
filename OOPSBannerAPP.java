3b4951e2039ad4f866f94ac7ba6fd18022be5496
sriramsaimedapati <sriramsaimedapati@gmail.com>
2026-02-17 19:02:23 +0530

    UC1: Print OOPS banner

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -6,7 +6,7 @@
  * @author Sriram
  * @version 1.0
  */
-public class OOPSBannerApp {
+public class OOPSBannerAPP{
     
     public static void main(String[] args) {
         // Print the literal "OOPS"
